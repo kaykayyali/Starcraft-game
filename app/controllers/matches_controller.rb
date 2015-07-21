2f75d1ad76ab0e0548248fc08f1a5569e6b9bb8f
Kaysser Kayyali <kaykayyali@gmail.com>
2015-07-21 17:10:16 -0400

    Added player profile with percentages

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -11,24 +11,31 @@ class MatchesController < ApplicationController
   def get_player
 
     player = Player.find_by(:id => params[:player])
-
     matches = Match.where("winner_id = " + player.id.to_s + " OR loser_id = " + player.id.to_s)
-
-    render :json => matches
+    win_percentage = playerPercentageMaker(player.id, matches)
+    loss_percentage = (100.0 - win_percentage).to_s + "%"
+    win_percentage = win_percentage.to_s + "%"
+    render :json =>{
+    	:percentages => {
+    				:wins => win_percentage,
+    				:losses => loss_percentage
+    				},
+    	:matches => matches		
+   		 }
   end
 
   def get_faction
 
     matches = Match.where(" winner_faction = ? OR loser_faction = ?",  params[:faction], params[:faction])
-    percentage = percentageMaker(params[:faction], matches)
+    percentage = factionPercentageMaker(params[:faction], matches)
     render :json => {
     	:win_percentage => percentage,
-    		:matches => matches
+    	:matches => matches
     		
     	}
   end
   private
-  def percentageMaker(faction,matches_array)
+  def factionPercentageMaker(faction,matches_array)
 		
 
 		wins = 0
@@ -37,17 +44,28 @@ class MatchesController < ApplicationController
 		matches_array.each do |match|
 			if match.winner_faction == faction
 				wins = wins + 1
-
 			else
 				losses = losses + 1
 			end
 		end
-		p wins
-		p losses
 		percentage = (wins.to_f / matches_array.count.to_f) * 100
-		p percentage
-		p matches_array.count
-
 		return percentage.to_s + "%"
+	end
+
+	  def playerPercentageMaker(player_id,matches_array)
+		
+
+		wins = 0
+		losses = 0
+
+		matches_array.each do |match|
+			if match.winner_id == player_id
+				wins = wins + 1
+			else
+				losses = losses + 1
+			end
+		end
+		percentage = (wins.to_f / matches_array.count.to_f) * 100
+		return percentage
 	end
 end
